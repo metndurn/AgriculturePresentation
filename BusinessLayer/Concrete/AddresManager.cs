b262066ebc870bb49ca1b8180c20a4441d7ae482
metndurn <mtndrn123@gmail.com>
2025-05-30 03:35:50 +0300

    adres alanları eklemesi yapılıyor yarıda bırakıldı

---
+++ BusinessLayer/Concrete/AddresManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccesLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class AddresManager : IAddressService // IAddressService interface'ini implement eden AddresManager sınıfı
+	{
+		private readonly IAddressDal _addresDal;
+
+		public AddresManager(IAddressDal addresDal)
+		{
+			_addresDal = addresDal;
+		}
+
+		public void Delete(Address t)
+		{
+			_addresDal.Delete(t);
+		}
+
+		public Address GetById(int id)
+		{
+			return _addresDal.GetById(id);
+		}
+
+		public List<Address> GetListAll()
+		{
+			return _addresDal.GetListAll();
+		}
+
+		public void Insert(Address t)
+		{
+			_addresDal.Insert(t);
+		}
+
+		public void Update(Address t)
+		{
+			_addresDal.Update(t);
+		}
+	}
+}
---
+++ BusinessLayer/ValidationRules/AddressValidator.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+	public class AddressValidator : AbstractValidator<Address>
+	{
+		public AddressValidator()
+		{
+			RuleFor(x=>x.Description1).NotEmpty().WithMessage("Açıklaması 1 boş geçilemez");
+			RuleFor(x=>x.Description2).NotEmpty().WithMessage("Açıklaması 2 boş geçilemez");
+			RuleFor(x=>x.Description3).NotEmpty().WithMessage("Açıklaması 3 boş geçilemez");
+			RuleFor(x=>x.Description4).NotEmpty().WithMessage("Açıklaması 4 boş geçilemez");
+			RuleFor(x=>x.Mapinfo).NotEmpty().WithMessage("Harita Bilgisi boş geçilemez");
+			RuleFor(x=>x.Description1).MaximumLength(30).WithMessage("Lütfen Açıklamayı Kısaltın");
+			RuleFor(x=>x.Description2).MaximumLength(30).WithMessage("Lütfen Açıklamayı Kısaltın");
+			RuleFor(x=>x.Description3).MaximumLength(30).WithMessage("Lütfen Açıklamayı Kısaltın");
+			RuleFor(x=>x.Description4).MaximumLength(30).WithMessage("Lütfen Açıklamayı Kısaltın");
+		}
+	}
+}
