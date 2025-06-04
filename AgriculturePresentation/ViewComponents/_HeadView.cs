86f346763522f5414fc3b95b0b2a24db2ea13484
metndurn <mtndrn123@gmail.com>
2025-06-04 23:16:54 +0300

    social media tarafına gecıs yapıldı ama kodlar yazılmadı

---
+++ AgriculturePresentation/ViewComponents/_HeadView.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+	/*bız bu yapıyı aynı partial view gibi kullanırız ama burada metod uzerınden bagımsız kullanabılıyoruz*/
+	public class _HeadView : ViewComponent
+	{
+		// ViewComponent, Controller gibi çalışır ama ViewComponent içinde metodlar tanımlayabiliriz.
+		public IViewComponentResult Invoke()//burada sag tık yapıp addview eklenmez
+		{
+			return View();
+		}
+	}
+}
---
+++ AgriculturePresentation/ViewComponents/_TopAddressView.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+	public class _TopAddressView : ViewComponent
+	{
+		/*dinamik isler yapabilmek için bunlar tazılması lazım unutma*/
+		private readonly IAddressService _addressService;
+
+		public _TopAddressView(IAddressService addressService)
+		{
+			_addressService = addressService;
+		}
+
+		public IViewComponentResult Invoke()//listeleme yapıyoruz
+		{
+			var values = _addressService.GetListAll();
+			return View(values);
+		}
+	}
+}
