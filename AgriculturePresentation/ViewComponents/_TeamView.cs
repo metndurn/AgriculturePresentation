884aecf8e2d29c40d6478bf1772c64de340f283f
metndurn <mtndrn123@gmail.com>
2025-07-06 01:55:19 +0300

    duyurular kısmına geldık

---
+++ AgriculturePresentation/ViewComponents/_TeamView.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+	public class _TeamView : ViewComponent
+	{
+		private readonly ITeamService _teamService; //tablolara buradan erısım saglandı
+		public _TeamView(ITeamService teamService) //erisim saglandıgı yerden servisi alıyoruz
+		{
+			_teamService = teamService;
+		}
+		public IViewComponentResult Invoke() /*listeleme alanı oldu*/
+		{
+			var values = _teamService.GetListAll();
+			return View(values);
+		}
+	}
+}
