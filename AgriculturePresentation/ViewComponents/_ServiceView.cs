e0107a3c94823529c2a6e71dc97c30bdfdbfa543
metndurn <mtndrn123@gmail.com>
2025-07-06 01:23:09 +0300

    team alanına gectık

---
+++ AgriculturePresentation/ViewComponents/_ServiceView.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+	public class _ServiceView : ViewComponent
+	{
+		private readonly IServiceService _serviceService; //tablolara buradan erısım saglandı
+
+		public _ServiceView(IServiceService serviceService)//erisim saglandıgı yerden servisi alıyoruz
+		{
+			_serviceService = serviceService;
+		}
+		public IViewComponentResult Invoke() /*listeleme alanı oldu*/
+		{
+			var values = _serviceService.GetListAll();
+			return View(values);
+		}
+	}
+}
---
+++ DataAccesLayer/Migrations/20250705215209_mig_add_service_column_class.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccesLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_service_column_class : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Class1",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogoImage",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Class1",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "LogoImage",
+                table: "Services");
+        }
+    }
+}
