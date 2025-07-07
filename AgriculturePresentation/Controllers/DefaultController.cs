c4e3c5fcc920edf0fae6d8bc11db5bd068d1c3b9
metndurn <mtndrn123@gmail.com>
2025-07-08 00:43:46 +0300

    admin login formuna giris yapıldı ve bırakıldı

--- AgriculturePresentation/Controllers/DefaultController.cs
+++ AgriculturePresentation/Controllers/DefaultController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AgriculturePresentation.Controllers
 {
 	public class DefaultController : Controller
 	{
+		private readonly IContactService _contactService;//tablolara buradan erısım saglandı
+
+		public DefaultController(IContactService contactService)// erisim saglandıgı yerden servisi alıyoruz
+		{
+			_contactService = contactService;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
+		}
+		[HttpGet]
+		public PartialViewResult SendMessage()//bunun amacı, mesaj gonderme alanını acmak
+		{
+			return PartialView();
+		}
+		[HttpPost]
+		public IActionResult SendMessage(Contact contact)//bu metot, mesaj gonderme alanından gelen verileri alır
+		{
+			// burada contact nesnesini kullanarak veritabanına kaydetme işlemi yapılabilir
+			// Örneğin: _contactService.Add(contact);
+			contact.Date = DateTime.Parse(DateTime.Now.ToShortDateString());//tarih kısmını alır
+			_contactService.Insert(contact);//contact nesnesini veritabanına ekler
+			return RedirectToAction("Index","Default");
+		}
+		public PartialViewResult ScriptView()//bunun amacı, mesaj gonderme alanındaki javascript kodlarını cagırmak
+		{
+			return PartialView();
 		}
 	}
 }
---
+++ AgriculturePresentation/ViewComponents/_MapView.cs
@@ -0,0 +1,16 @@
+﻿using DataAccesLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+	public class _MapView : ViewComponent
+	{
+		public IViewComponentResult Invoke()/*listeleme alanı oldu*/
+		{
+			AgricultureContext agricultureContext = new AgricultureContext();
+			var values = agricultureContext.Addresses.Select(x=>x.Mapinfo).FirstOrDefault();
+			ViewBag.map = values;
+			return View();
+		}
+	}
+}
