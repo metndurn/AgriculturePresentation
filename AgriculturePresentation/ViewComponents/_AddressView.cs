0ac4fd55ed97e37b3653d5c686d28d51cf491931
metndurn <mtndrn123@gmail.com>
2025-07-07 23:38:01 +0300

    mesaj gonderme ıslemıne gectık orada takılı kaldım

---
+++ AgriculturePresentation/ViewComponents/_AddressView.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+	public class _AddressView : ViewComponent
+	{
+		private readonly IAddressService _addressService;
+
+		public _AddressView(IAddressService addressService)//tablolara buradan erişim sağlandı
+		{
+			_addressService = addressService;
+		}
+
+		public IViewComponentResult Invoke() //listeleme alanı oldu
+		{
+			var values = _addressService.GetListAll();
+			return View(values);
+		}
+	}
+}
---
+++ AgriculturePresentation/ViewComponents/_ContactView.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+	public class _ContactView : ViewComponent
+	{
+		private readonly IContactService _contactService;//tablolara buradan erısım saglandı
+		public _ContactView(IContactService contactService)//erisim saglandıgı yerden servisi alıyoruz
+		{
+			_contactService = contactService;
+		}
+		public IViewComponentResult Invoke()/*listeleme alanı oldu*/
+		{
+			var values = _contactService.GetListAll();
+			return View(values);
+		}
+	}
+}
