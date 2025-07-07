897646bca0479197bfe4431478819017146b438b
metndurn <mtndrn123@gmail.com>
2025-07-07 20:09:50 +0300

    galeri kısmında kaldık vıdeo yarıda

---
+++ AgriculturePresentation/ViewComponents/_AnnouncementView.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+	public class _AnnouncementView : ViewComponent
+	{
+		private readonly IAnnouncementService _announcementService; //tablolara buradan erişim sağlandı
+
+		public _AnnouncementView(IAnnouncementService announcementService)
+		{
+			_announcementService = announcementService;
+		}
+		public IViewComponentResult Invoke() //listeleme alanı oldu
+		{
+			var values = _announcementService.GetListAll();
+			return View(values);
+		}
+	}
+}
---
+++ AgriculturePresentation/ViewComponents/_GalleryView.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+	public class _GalleryView : ViewComponent/*gallery ve image aynıdır unutma*/
+	{
+		private readonly IImageService _imageService; //tablolara buradan erişim sağlandı
+
+		public _GalleryView(IImageService imageService)
+		{
+			_imageService = imageService;
+		}
+		public IViewComponentResult Invoke() //listeleme alanı oldu
+		{
+			var values = _imageService.GetListAll();
+			return View(values);
+		}
+	}
+}
